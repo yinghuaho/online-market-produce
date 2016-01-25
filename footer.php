52ed797df0db265e8e7f98f34e0866d43c6213f6
diachion <dia@lydias-mbp.wifi.bcit.ca>
2016-01-24 18:31:34 -0800

    Added icons in footer
    Added the Payment Methods and Social Media icons to the footer

--- footer.php
+++ footer.php
@@ -1,31 +1,30 @@
 <footer metal:define-macro="page_footer">
 
 <div class="container">
-	<div class="col-md-4">
-		<ul>
+	<div class="col-md-4 footer-content">
+		<ul class="footer-list">
 			<li><a href="#">About Us</a></li>
 			<li><a href="#">Contact Us</a></li>
 		</ul>
 	</div>
 
-	<div class="col-md-4 text-center">
+	<div class="col-md-4 footer-content">
 		<h4>Payment Methods</h4>
 		<ul class="list-inline">
-			<li><span class="glyphicon glyphicon-credit-card"></span></li>
-			<li><span class="glyphicon glyphicon-credit-card"></span></li>
-			<li><span class="glyphicon glyphicon-credit-card"></span></li>
-			<li><span class="glyphicon glyphicon-credit-card"></span></li>
+			<li><img class="footer-icons" src="./imgs/visa.svg" alt="VISA"/></li>
+			<li><img class="footer-icons" src="./imgs/mastercard.svg" alt="MASTERCARD"/></li>
+			<li><img class="footer-icons" src="./imgs/paypal.svg" alt="PayPal"/></li>
 		</ul>
 	</div>
 
-	<div class="col-md-4 text-right">
+	<div class="col-md-4 footer-content">
 		<h4>Follow Us On</h4>
 		<ul class="list-inline">
-			<li><span class="glyphicon glyphicon-thumbs-up"></span></li>
-			<li><span class="glyphicon glyphicon-thumbs-up"></span></li>
-			<li><span class="glyphicon glyphicon-thumbs-up"></span></li>
-			<li><span class="glyphicon glyphicon-thumbs-up"></span></li>
-			<li><span class="glyphicon glyphicon-thumbs-up"></span></li>
+			<li><img class="footer-icons" src="./imgs/facebook.svg" alt="Facebook"/></li>
+			<li><img class="footer-icons" src="./imgs/twitter.svg" alt="Twitter"/></li>
+			<li><img class="footer-icons" src="./imgs/instagram.svg" alt="Instagram"/></li>
+			<li><img class="footer-icons" src="./imgs/tumblr.svg" alt="tumblr"/></li>
+			<li><img class="footer-icons" src="./imgs/pinterest.svg" alt="Pinterest"/></li>
 		</ul>
 	</div>
 
