97464fc279acb2205aa70eb083d3d89f86525547
diachion <dia@lydias-mbp.wifi.bcit.ca>
2016-02-01 11:56:42 -0800

    Added more to cart page
    Added some stuff to cart.php

--- cart.php
+++ cart.php
@@ -14,6 +14,60 @@
 
 <div id="supertop"></div>
 
+<div class="container">
+  <h1 class="text-center">Fresh 'n Healthy: Online Farmers' Market</h1>
+
+  <div class="col-md-5"> <h2 class="bg-warning text-center">Shopping Cart</h2> </div>
+
+  <div class="col-md-12 text-center item-bg-color">
+
+    <div class="col-md-2">
+      <h4>Product</h4>
+        <div class="cart-card-thumbnail">
+          <img src="img_features/slider2.jpg" alt="veggies"/>
+        </div>
+        <h5>Onions</h5>
+        <h6>Onions make you cry. Too many ninjas chopping onions around me</h6>
+    </div>
+
+    <div class="col-md-3">
+      <h4>Quantity</h4>
+        <div class="col-md-8 col-md-offset-2">
+            <!-- <strong class="card-qty">QTY.</strong> -->
+              <div class="input-group">
+                <div class="input-group-btn">
+                  <button class="btn btn-success">-</button>
+                </div>
+
+                <input type="text" class="form-control text-center" value="1"/>
+
+                <div class="input-group-btn">
+                  <button class="btn btn-success">+</button>
+                </div>
+              </div>
+        </div>  <!-- COL MD 6 -->
+    </div>
+
+    <div class="col-md-1">
+      x
+    </div>
+
+    <div class="col-md-2">
+      <h4>Unit Price</h4>
+    </div>
+
+    <div class="col-md-2">
+      <h4>Subtotal</h4>
+    </div>
+
+    <div class="col-md-2">
+      remove
+    </div>
+
+  </div> <!-- column -->
+
+</div> <!-- container --> 
+
 
 
 <!-- ================================== FOOTER ================================== -->
