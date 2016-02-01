4280bc0b54238b1961612abf696addbc0fd819bd
diachion <dia@lydias-mbp.wifi.bcit.ca>
2016-02-01 11:02:11 -0800

    Added cart.php pages and fixed disable button
    Added: cart.php & cartphptal.php | Fixed disable pagination button

---
+++ cart.php
@@ -0,0 +1,29 @@
+<html>
+  <headReplace metal:use-macro="header.php/meta_head">
+  </headReplace>
+<body>
+
+<!-- ================================== NAVIGATION ================================== -->
+
+<navReplace metal:use-macro="nav.php/nav_bar">
+</navReplace>
+
+
+
+<!-- ================================== SHOPPING CART ================================== -->
+
+<div id="supertop"></div>
+
+
+
+<!-- ================================== FOOTER ================================== -->
+<footerReplace metal:use-macro="footer.php/page_footer">
+</footerReplace>
+    
+<div class="top"><a href="#supertop"><span class="glyphicon glyphicon-triangle-top"></span></a></div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script type="text/javascript" src="lib/js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ cartphptal.php
@@ -0,0 +1,25 @@
+<?php
+
+
+    require_once('./lib/PHPTAL-1.3.0/PHPTAL.php');
+
+    // render the whole page using PHPTAL
+
+    // finally, create a new template object
+    $template = new PHPTAL('cart.php');
+
+    // now add the variables for processing and that you created from above:
+    $template->page_title = "Fresh 'n Healthy: Online Farmers' Market";
+
+
+    // execute the template
+    try {
+        echo $template->execute();
+    }
+    catch (Exception $e){
+        // not much else we can do here if the template engine barfs
+        echo $e;
+    }
+
+
+?>
