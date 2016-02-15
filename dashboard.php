a484d2f0b17ad507658ba3d6e353d932a5bc33cf
diachion <dia@lydias-mbp.wifi.bcit.ca>
2016-02-15 12:18:04 -0800

    Added dashboard.php
    Added and started on the page

---
+++ dashboard.php
@@ -0,0 +1,42 @@
+<html> 
+  <head>
+    <title>Fresh 'n Healthy: Dashboard</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="stylesheet" type="text/css" href="lib/css/bootstrap.min.css"></link>
+    <link rel="stylesheet" type="text/css" href="css/products.css"></link>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script type="text/javascript" src="lib/js/bootstrap.min.js"></script>
+  </head>
+
+
+<body>
+
+<div class="container-fluid">
+  <div class="col-xs-2">
+    <div class="list-group">
+      <span class="list-group-item active">
+        Categories
+      </span>
+      <a href="#" class="list-group-item">Organic Fruits</a>
+      <a href="#" class="list-group-item">Organic Vegetables</a>
+      <a href="#" class="list-group-item">Dairy</a>
+      <a href="#" class="list-group-item">Meats</a>
+      <a href="#" class="list-group-item">Other</a>
+    </div>
+
+    <div class="list-group">
+      <a href="#" class="list-group-item">Back to Home</a>
+      <a href="#" class="list-group-item">Logout</a>
+    </div>
+  </div> <!-- column -->
+
+  <div class="col-xs-10">
+    <div id="display">
+      HUAHUA HO HO HO HO H IKE UA O FOR SALE
+    </div>
+  </div> <!-- column -->
+</div> <!-- container -->
+
+</body>
+</html>
